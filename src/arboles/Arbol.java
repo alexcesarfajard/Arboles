682fbaac671e567a878760196a48125e9442c677
alexcesarfajard <acesar23616@ufide.ac.cr>
2023-11-15 21:30:13 -0600

    se crean todos los metodos

--- src/arboles/Arbol.java
+++ src/arboles/Arbol.java
@@ -1,20 +1,23 @@
 package arboles;
 
 public class Arbol {
-    
+
     private Nodo raiz;
-    
-    public void insertar(int x){ //primer paso para insertar en el arbol
-        if (raiz == null){ //si no hay elementos en el arbol, se crea la raiz 
+    int resultado = 0;
+    int nivelactual = 0;
+    int nivelarbol;
+
+    public void insertar(int x) { //primer paso para insertar en el arbol
+        if (raiz == null) { //si no hay elementos en el arbol, se crea la raiz 
             raiz = new Nodo(x);
         } else {
             insertarRecursivo(raiz, x);
         }
     }
-    
-    public void insertarRecursivo(Nodo n, int x){
-        if (x<n.getId()){ // esta validacion es para saber si debemos insertar a la izquierda
-            if (n.getHijoizq() == null){ // validamos si el hijo izquierdo tiene algo, si no hay, lo creamos
+
+    private void insertarRecursivo(Nodo n, int x) {
+        if (x < n.getId()) { // esta validacion es para saber si debemos insertar a la izquierda
+            if (n.getHijoizq() == null) { // validamos si el hijo izquierdo tiene algo, si no hay, lo creamos
                 n.setHijoizq(new Nodo(x)); //se crea el hijo izquierdo
             } else {
                 insertarRecursivo(n.getHijoizq(), x);
@@ -23,9 +26,181 @@ public class Arbol {
             if (n.getHijoder() == null) { //validamos si el hijo derecho tiene algo, si no hay, lo creamos
                 n.setHijoder(new Nodo(x)); //se crea el hijo derecho
             } else { // si el hijo derecho tiene algo, llamamos recursivamente hasta encontrar que no haya nada 
-                insertarRecursivo(n.getHijoder(), x);   
+                insertarRecursivo(n.getHijoder(), x);
+            }
+        }
+    }
+
+    public void inorden() { //imprimir el arbol en inorden
+        if (raiz != null) {
+            inordenRecursivo(raiz);
+        } else {
+            System.out.println("No hay nada en la raiz, el arbol está vacío");
+        }
+    }
+
+    private void inordenRecursivo(Nodo n) { //metodo recursivo para mostrar arbol inorden
+        if (n != null) {
+            inordenRecursivo(n.getHijoizq());
+            System.out.print(n.getId() + ", ");
+            inordenRecursivo(n.getHijoder());
+        }
+    }
+
+    public void preorden() { //imprimir el arbol en preorden
+        if (raiz != null) {
+            preordenRecursivo(raiz);
+        } else {
+            System.out.println("No hay nada en la raiz, el arbol está vacío");
+        }
+    }
+
+    private void preordenRecursivo(Nodo n) { //metodo recursivo para mostrar arbol preorden
+        if (n != null) {
+            System.out.print(n.getId() + ", ");
+            inordenRecursivo(n.getHijoizq());
+            inordenRecursivo(n.getHijoder());
+        }
+    }
+
+    public void posorden() { //imprimir el arbol en posorden
+        if (raiz != null) {
+            posordenrecursivo(raiz);
+        } else {
+            System.out.println("No hay nada en la raiz, el arbol está vacío");
+        }
+    }
+
+    private void posordenrecursivo(Nodo n) { //metodo recursivo para mostrar arbol posorden
+        if (n != null) {
+            posordenrecursivo(n.getHijoizq());
+            posordenrecursivo(n.getHijoder());
+            System.out.print(n.getId() + ", ");
+        }
+    }
+
+    //imprimir menor numero en el arbol
+    public void imprimeMenor() {
+        if (raiz != null) {
+            System.out.println("El numero menor en el arbol es: " + imprimeMenorRecursivo(raiz));
+        } else {
+            System.out.println("No hay nada en la raiz, el arbol está vacío");
+        }
+    }
+
+    private int imprimeMenorRecursivo(Nodo n) {
+
+        if (resultado == 0) {
+            resultado = n.getId();
+        }
+
+        if (n != null) {
+            imprimeMenorRecursivo(n.getHijoizq());
+            imprimeMenorRecursivo(n.getHijoder());
+
+            if (n.getId() < resultado) {
+                resultado = n.getId();
+            }
+        }
+
+        return resultado;
+
+    }
+
+    //imprimir hojas (los nodos que no tengan hijos
+    public void mostrarhojas() {
+        if (raiz != null) {
+            mostrarHojasRecursivo(raiz);
+        } else {
+            System.out.println("No hay nada en la raiz, el arbol está vacío");
+        }
+    }
+
+    private void mostrarHojasRecursivo(Nodo n) {
+
+        if (n != null) {
+
+            if (n.getHijoder() == null && n.getHijoizq() == null) {
+                System.out.println(n.getId());
+            } else {
+                mostrarHojasRecursivo(n.getHijoder());
+                mostrarHojasRecursivo(n.getHijoizq());
+            }
+        }
+    }
+
+    //imprimir nivel n del arbol
+    public void buscarnivel(int nivelrequerido) {
+        System.out.println("Datos del nivel: " + nivelrequerido);
+        if (raiz != null) {
+            buscarnivelrecursivo(raiz, nivelactual, nivelrequerido);
+        } else {
+            System.out.println("No hay nada en la raiz, el arbol está vacío");
+        }
+    }
+
+    private void buscarnivelrecursivo(Nodo n, int nivelactual, int nivelrequerido) {
+
+        if (n != null) {
+            if (nivelrequerido == nivelactual) {
+                System.out.println(n.getId());
+            } else {
+                buscarnivelrecursivo(n.getHijoizq(), nivelactual + 1, nivelrequerido);
+                buscarnivelrecursivo(n.getHijoder(), nivelactual + 1, nivelrequerido);
+            }
+        }
+
+    }
+
+    // imprimir altura del arbol 
+    public int cantidadniveles(){
+        return cantidadnivelesrecursivo(raiz);
+    }
+    
+    private int cantidadnivelesrecursivo(Nodo n){
+        if (n == null){
+            return 0;
+        } else {
+            int alturaizq = cantidadnivelesrecursivo(n.getHijoizq());
+            int alturader = cantidadnivelesrecursivo(n.getHijoder());
+            
+            if (alturaizq > alturader){
+                return alturaizq + 1;
+            } else {
+                return alturader + 1;
             }
         }
     }
     
+    /*
+    public void mostrarnivelarbol() {
+        if (raiz != null) {
+            System.out.println("El nivel actual del arbol es: " + mostrarnivelarbolrecursivo(raiz));
+        } else {
+            System.out.println("No hay nada en la raiz, el arbol está vacío");
+        }
+    }
+    int alturaizq;
+    int alturader;
+    
+    private int mostrarnivelarbolrecursivo(Nodo n) {
+
+        if (n != null) {
+            alturaizq += mostrarnivelarbolrecursivo(n.getHijoizq());
+            alturader += mostrarnivelarbolrecursivo(n.getHijoder());
+
+            if(alturaizq > alturader){
+                nivelarbol = alturaizq + 1;
+            } else {
+                if(alturader > alturaizq){
+                    nivelarbol = alturader + 1;
+                }
+            }
+  
+        }
+        
+        return nivelarbol;
+        
+    }*/
+
 }
--- src/arboles/Arboles.java
+++ src/arboles/Arboles.java
@@ -1,14 +1,13 @@
 package arboles;
 
-
 public class Arboles {
 
     public static void main(String[] args) {
         // TODO code application logic here
         //System.out.println("Hello World");
-        
+
         Arbol arbol = new Arbol();
-        
+
         arbol.insertar(50);
         arbol.insertar(20);
         arbol.insertar(80);
@@ -21,7 +20,31 @@ public class Arboles {
         arbol.insertar(33);
         arbol.insertar(82);
         
+        /*
+        System.out.println("Recorriendo arbol inorden");
+        arbol.inorden();
+
+        System.out.println("\nRecorriendo arbol preorden");
+        arbol.preorden();
+
+        System.out.println("\nRecorriendo arbol posorden");
+        arbol.posorden();
+
+        System.out.println("\nBuscando el menor valor dentro del arbol");
+        arbol.imprimeMenor();
+        
+        
+        System.out.println("\nBuscando solamente las hojas");
+        arbol.mostrarhojas();
+            
+        
+        System.out.println("\nBuscando los datos del nivel n");
+        arbol.buscarnivel(2);
+        */
+        
+        System.out.println("\nMostrando la cantidad de niveles del arbol");
+        System.out.println("Cantidad niveles del arbol: " + arbol.cantidadniveles());
         
     }
-    
+
 }
