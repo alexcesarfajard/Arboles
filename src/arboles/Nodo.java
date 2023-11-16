d94ef63bc9eb6c63f7b81202c2f0910656e9ba40
alexcesarfajard <acesar23616@ufide.ac.cr>
2023-11-15 18:41:51 -0600

    primer cambio

---
+++ src/arboles/Nodo.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arboles;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Nodo {
+    
+    private int id;
+    private Nodo hijoizq;
+    private Nodo hijoder;
+    
+    public Nodo(int id){
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Nodo getHijoizq() {
+        return hijoizq;
+    }
+
+    public void setHijoizq(Nodo hijoizq) {
+        this.hijoizq = hijoizq;
+    }
+
+    public Nodo getHijoder() {
+        return hijoder;
+    }
+
+    public void setHijoder(Nodo hijoder) {
+        this.hijoder = hijoder;
+    }
+    
+    
+    
+}
